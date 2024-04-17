d289dc4aad656f91aa7c31fe5cefc489df072256
Nikolaos Bermparis <93055498+BERBARIANKING@users.noreply.github.com>
2024-04-17 15:00:42 +0300

    Create synonyms.py

---
+++ mathima/synonyms.py
@@ -0,0 +1,20 @@
+#This program helps with synonyms 
+#For example it can find synonyms for the word action etc.
+#So this can help woith the genres
+
+from nltk.corpus import wordnet 
+
+def find_synonyms(word):
+    synonyms = []
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            synonyms.append(lemma.name())
+    return set(synonyms)
+
+if __name__ == '__main__':
+    word = input("Input is the word: ")
+    sysnonyms = find_synonyms(word)
+    if sysnonyms:
+        print(f"The synonyms of \"{word}\" are: {', '.join(sysnonyms)}")
+    else:
+        print(f"No synonyms found for the \"{word}\".")
