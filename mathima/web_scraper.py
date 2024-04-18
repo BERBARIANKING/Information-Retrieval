f6300cb03107e1094d6ad94cac1765d9dbdfa779
Nikolaos Bermparis <93055498+BERBARIANKING@users.noreply.github.com>
2024-04-18 11:13:24 +0300

    Update web_scraper.py

--- mathima/web_scraper.py
+++ mathima/web_scraper.py
@@ -1,6 +1,9 @@
 import requests 
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
+
+#synarthsh gia katebasma keimenou apo url
 def download_text_from_url(url):
     #katebase thn istoselida
     response = requests.get(url)
@@ -17,6 +20,24 @@ def download_text_from_url(url):
          print("Failed to download page.")
          return None 
     
+
+#synarthsh gia thn euresh links se mia istoselida
+def get_links_from_url(url):
+    #katebase thn istoselida
+    response = requests.get(url)
+    #elegxos epityxias ths aithshs
+    if response.status_code == 200:
+        #analush tou html kwdika
+        soup = BeautifulSoup(response.text, "html.parser")
+        #euresh stoixeiwn pou periexoun links
+        links = soup.find_all("a", href=True)
+        #sygxwneush twn links
+        internal_links = [urljoin(response.url, link["href"]) for link in links if link["href"].startswith("/wiki/")]
+        return internal_links
+    else:
+        print("Failed to download page.")
+        return None
+
 def save_text_to_file(text, filename):
 #apothkeush tou keimenou se arxeio
     with open(filename , "w" , encoding="utf-8") as f:
@@ -25,9 +46,9 @@ def save_text_to_file(text, filename):
 
 if __name__ == "__main__":
     # orismos ths url istoselidas wikipedia gia thn glossa python
-    url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+    url = "https://icsd.aegean.gr"
     #katebasma keimenou apo url
     text = download_text_from_url(url)
     if text:
         #apothkeush tou keimenou se arxeio
-        save_text_to_file(text, "python_wikipedia_text.txt")
+        save_text_to_file(text, "python_icsd_text.txt")
